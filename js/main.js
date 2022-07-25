b6f9231c01eea77e79bd356a41094e440d80cd51
Alaa Yasser <74079336+alaayasser17@users.noreply.github.com>
2022-07-25 21:17:53 +0200

    add score &check answer

--- js/main.js
+++ js/main.js
@@ -10,27 +10,26 @@ let correctAnswer = "",
   correcrScore = (askedCount = 0),
   totalQuestion = 20;
 
-
-
 async function loadQuist() {
   const url = "https://opentdb.com/api.php?amount=20&category=21";
   const result = await fetch(`${url}`);
   const data = await result.json();
-  _result.innerHTML="";
+  _result.innerHTML = "";
   showQuestion(data.results[0]);
-  
 }
 function eventListener() {
   _checkBtn.addEventListener("click", checkAnswer);
+  _againBtn.addEventListener("click", restartQuiz);
 }
-  _result.innerHTML=[]
-document.addEventListener("DOMContentLoaded",function(){
+_result.innerHTML = [];
+document.addEventListener("DOMContentLoaded", function () {
   loadQuist();
   eventListener();
   _totalQuestion.textContent = totalQuestion;
   _correctScore.textContent = correcrScore;
 });
 function showQuestion(data) {
+  _checkBtn.disabled = false;
   correctAnswer = data.correct_answer;
   let incorrectAnswer = data.incorrect_answers;
   let optionslist = incorrectAnswer;
@@ -42,11 +41,10 @@ function showQuestion(data) {
   _question.innerHTML = `${data.question}<br><span class="category">${data.category}</span>`;
   _options.innerHTML = `${optionslist
     .map((option, index) => `<li>${index + 1}.<span> ${option} </span></li>`)
-    .join('')}`;
+    .join("")}`;
   selectOption();
 }
 function selectOption() {
-  
   _options.querySelectorAll("li").forEach((option) => {
     option.addEventListener("click", () => {
       if (_options.querySelector(".selected")) {
@@ -56,8 +54,7 @@ function selectOption() {
       option.classList.add("selected");
     });
   });
-  console.log(correctAnswer)
-
+  console.log(correctAnswer);
 }
 
 function checkAnswer() {
@@ -66,13 +63,42 @@ function checkAnswer() {
     let selectedAnswer = _options.querySelector(".selected span").textContent;
     if (selectedAnswer.trim() == HTMLDecode(correctAnswer)) {
       correcrScore++;
-      _result.innerHTML = `<p><i class = "fas fa-check"></i>Correct Answer!</p>`;
+      _result.innerHTML = `<p><i class="fa-solid fa-circle-check"></i>Correct Answer!</p>`;
     } else {
-      _result.innerHTML = `<p><i class = "fas fa-times"></i>Incorrect Answer!</p> <small><b>Correct Answer: </b>${correctAnswer}</small>`;
-      }
+      _result.innerHTML = `<p><i class="fa-solid fa-circle-xmark"></i>Incorrect Answer!</p> <small><b>Correct Answer: </b>${correctAnswer}</small>`;
+    }
+    checkCount();
+  } else {
+    _result.innerHTML = `<p><i class="fa-solid fa-circle-question"></i>Please select an option!</p>`;
+    _checkBtn.disabled = false;
   }
 }
-function HTMLDecode(textString){
-  let doc =new DOMParser().parseFromString(textString,'text/html')
+function HTMLDecode(textString) {
+  let doc = new DOMParser().parseFromString(textString, "text/html");
   return doc.documentElement.textContent;
+}
+function checkCount() {
+  askedCount++;
+  setCount();
+  if (askedCount == totalQuestion) {
+    _result.innerHTML += `<p>Your Score is ${correcrScore}.</p>`;
+    _againBtn.style.display = "block";
+    _checkBtn.style.display = "none";
+  } else {
+    setTimeout(() => {
+      loadQuist();
+    }, 300);
+  }
+}
+function setCount() {
+  _totalQuestion.textContent = totalQuestion;
+  _correctScore.textContent = correcrScore;
+}
+function restartQuiz() {
+  correcrScore = askedCount = 0;
+  _againBtn.style.display = "none";
+  _checkBtn.style.display = "block";
+  _checkBtn.disabled = false;
+  setCount();
+  loadQuist();
 }
